264d5d1054d2d8c78f24f32e32d172577f79f118
Emilio Navarro <sainfuw@outlook.com>
2024-09-14 11:59:53 -0300

    WIP: layout

---
+++ src/db/data.ts
@@ -0,0 +1,112 @@
+export const heroes = [
+  {
+    id: 1,
+    name: 'Thunderbolt',
+    role: 'Mago',
+    description: 'Un poderoso mago que controla los rayos y las tormentas.',
+  },
+  {
+    id: 2,
+    name: 'Iron Guardian',
+    role: 'Tanque',
+    description: 'Un guerrero imparable con una armadura impenetrable.',
+  },
+  {
+    id: 3,
+    name: 'Shadow Assassin',
+    role: 'Asesino',
+    description: 'Un asesino sigiloso que ataca desde las sombras.',
+  },
+  {
+    id: 4,
+    name: "Nature's Wrath",
+    role: 'Apoyo',
+    description:
+      'Un druida que canaliza el poder de la naturaleza para ayudar a sus aliados.',
+  },
+  {
+    id: 5,
+    name: 'Flame Dancer',
+    role: 'Mago',
+    description: 'Una hechicera que domina el arte del fuego y la danza.',
+  },
+  {
+    id: 6,
+    name: 'Frost Giant',
+    role: 'Tanque',
+    description: 'Un gigante de hielo que congela a sus enemigos con su toque.',
+  },
+  {
+    id: 7,
+    name: 'Techno Wizard',
+    role: 'Apoyo',
+    description:
+      'Un genio que combina magia y tecnología para potenciar a sus aliados.',
+  },
+  {
+    id: 8,
+    name: 'Void Walker',
+    role: 'Asesino',
+    description:
+      'Un ser enigmático que se mueve entre dimensiones para eliminar a sus objetivos.',
+  },
+]
+
+export const items = [
+  {
+    id: 1,
+    name: 'Espada del Trueno',
+    type: 'Arma',
+    price: 1000,
+    description: 'Una espada que canaliza el poder del rayo.',
+  },
+  {
+    id: 2,
+    name: 'Amuleto de Protección',
+    type: 'Accesorio',
+    price: 750,
+    description: 'Un amuleto que aumenta la defensa mágica.',
+  },
+  {
+    id: 3,
+    name: 'Botas de Velocidad',
+    type: 'Armadura',
+    price: 800,
+    description: 'Botas que aumentan la velocidad de movimiento.',
+  },
+  {
+    id: 4,
+    name: 'Orbe de Poder',
+    type: 'Artefacto',
+    price: 1200,
+    description: 'Un orbe que amplifica el poder mágico.',
+  },
+  {
+    id: 5,
+    name: 'Capa de Invisibilidad',
+    type: 'Armadura',
+    price: 950,
+    description: 'Una capa que otorga invisibilidad temporal.',
+  },
+  {
+    id: 6,
+    name: 'Bastón de Curación',
+    type: 'Arma',
+    price: 900,
+    description: 'Un bastón que permite curar a los aliados.',
+  },
+  {
+    id: 7,
+    name: 'Anillo de Mana',
+    type: 'Accesorio',
+    price: 600,
+    description: 'Un anillo que aumenta la regeneración de mana.',
+  },
+  {
+    id: 8,
+    name: 'Escudo Reflector',
+    type: 'Armadura',
+    price: 1100,
+    description: 'Un escudo que refleja parte del daño recibido.',
+  },
+]
