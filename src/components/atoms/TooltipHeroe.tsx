fceda1cbf99728536f08a7897970c65f1f5ba80b
Emilio Navarro <sainfuw@outlook.com>
2024-09-23 16:54:37 -0300

    hero cards rework

--- src/components/atoms/TooltipHeroe.tsx
+++ src/components/atoms/TooltipHeroe.tsx
@@ -1,5 +1,5 @@
 import type { IHero } from '@/interfaces/hero'
-import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
+import { Card, CardContent } from '../ui/card'
 import {
   Tooltip,
   TooltipContent,
@@ -18,19 +18,21 @@ export const TooltipHeroe = ({ hero }: Props) => {
       <Tooltip>
         <TooltipTrigger asChild>
           <a href={`/hero/${hero.name}`}>
-            <Card className='hover:bg-black/5 dark:hover:bg-white/5 transition-colors bg-white dark:bg-black/20 border border-black/30 dark:border-white/20'>
-              <CardHeader className='p-0'>
-                <CardTitle className='text-md text-center py-2'>
-                  {hero.name}
-                </CardTitle>
-              </CardHeader>
+            <Card className='bg-white dark:bg-black/20 border border-black/30 dark:border-white/20 relative overflow-hidden'>
+              <div className='absolute -translate-x-1/2 left-1/2 z-10 top-2'>
+                {hero.name.split(' ').map((w) => (
+                  <h3 className='[text-shadow:_3px_3px_2px_rgb(0_0_0/_1)] text-xl text-center font-bold'>
+                    {w}
+                  </h3>
+                ))}
+              </div>
               <CardContent className='p-0'>
                 <img
                   src={imageURL}
                   alt={hero.name}
                   width={300}
                   height={200}
-                  className='rounded-lg'
+                  className='rounded-lg transition-all hover:scale-125'
                 />
               </CardContent>
             </Card>
